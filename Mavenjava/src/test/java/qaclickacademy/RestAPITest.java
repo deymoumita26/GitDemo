8ffb83fe6dc22c97ecf9df7efca7d5716b92c446
Moumita <dey.moumita26@gmail.com>
2019-06-21 12:20:56 +0530

    my first commit

---
+++ Mavenjava/src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	
+	@Test
+	public void PostJira()
+	
+	{
+		System.out.println("PostJira");
+	}
+	
+	@Test
+	public void DleteTwitter()
+	
+	{
+		System.out.println("DleteTwitter");
+	}
+}
---
+++ Mavenjava/src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void BrowserLaunch()
+	
+	{
+		System.out.println("BrowserLaunch");
+	}
+	
+	@Test
+	public void ElementsUI()
+	
+	{
+		System.out.println("ElementsUI");
+	}
+	
+}
